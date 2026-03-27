db9086b40caeb5ec65de1e504a9bf43c777e959f
iakorotkov <vanya.korotkov.2000@gmail.com>
2026-03-27 13:11:55 +0400

    001 - Подготовка инфраструктуры для сущностей

---
+++ Domain/Abstractions/IEntity.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Abstractions
+{
+    interface IEntity<T>: IEntity
+    {
+        public T Id { get; set; }
+    }
+    public interface IEntity
+    {
+    }
+}
