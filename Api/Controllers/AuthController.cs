544e423631108a55d49cf0f6c0da960294a6781b
iakorotkov <vanya.korotkov.2000@gmail.com>
2026-04-28 13:12:36 +0400

    021 - Security DTO + Auth login

---
+++ Api/Controllers/AuthController.cs
@@ -0,0 +1,32 @@
+﻿using Domain.Security;
+using Domain.Security.Dtos;
+using Microsoft.AspNetCore.Identity;
+
+namespace Api.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class AuthController(UserManager<CustomIdentityUser> manager) 
+    : ControllerBase
+{
+    [HttpPost("login")]
+    public async Task<IResult> Login(LoginRequestDto dto)
+    {
+        var person = await manager.FindByEmailAsync(dto.Email);
+        if (person == null) {
+            return Results.Unauthorized();
+        }
+        
+        var result = await manager.CheckPasswordAsync(person, dto.Password);
+        if (result)
+        {
+            var response = new IdentityUserResponseDto(
+                person.UserName!, person.Email!, "jwt"
+            );
+
+            return Results.Ok(new { result = response});
+        }
+
+        return Results.Unauthorized();
+    }
+}
